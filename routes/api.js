93787640c96852e8e9a5afd3b9f762667fa560a2
jaychan0125 <joycechan0125@gmail.com>
2023-05-24 12:41:13 -0400

    fixed post('/notes') endpoint to create notes

--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,13 @@ const db = require('../db/db.json')
 
 
 //GET ALL the notes saved as objects in db.json file
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     console.log(`HTTP METHOD: ${req.method}`);
     return res.status(200).json(db);
 });
 
 //CREATE new notes
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const { title, text } = req.body;
     console.log(`HTTP METHOD: ${req.method}`);
     //if the newNote has both a title and text:
@@ -28,7 +28,7 @@ router.post('/api/notes', (req, res) => {
         // res.status(200).json(db);
 
         //persist data into the db.json file
-        writeFile('/db/db.json', JSON.stringify(db), (err) =>
+        writeFile('./db/db.json', JSON.stringify(db), (err) =>
         err
           ? console.error(err)
           : console.log(
@@ -39,18 +39,19 @@ router.post('/api/notes', (req, res) => {
 });
 
 //HERE
-router.delete('/api/notes/:note_id', (req, res) => {
-    console.log(req.params.note_id)
+router.delete('/notes/:note_id', (req, res) => {
     const noteId = req.params.note_id;
-    readFile('/db/db.json', 'utf-8', (err, data) => {
+    readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Internal Server Error');
+        }
+
         let notes = JSON.parse(data);
-        //console.log(notes)
-       // console.log(noteId);
-       // console.log(notes.note_id)
-        const deleteNote = notes.findIndex(notes => notes.note_id === noteId);
+        const deleteNoteIndex = notes.findIndex(note => note.note_id === noteId);
         //findIndex, if no elements satisfy: -1,  otherwise will output the index number
-        console.log(deleteNote)
-        notes.splice(deleteNote, 1);
+        console.log(deleteNoteIndex)
+        // notes.splice(deleteNoteIndex, 1);
         //save new notes db
 
     })
