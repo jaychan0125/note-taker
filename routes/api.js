b0e4f23799e52285ff132a0ec1a652a67c01cf63
jaychan0125 <joycechan0125@gmail.com>
2023-05-25 17:46:35 -0400

    fixed the delete and post endpoints!

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,16 @@
 const router = require('express').Router();
-const path = require('path');
 const { writeFile, readFile } = require('fs');
 const { v1: uuidv1 } = require('uuid');
 const db = require('../db/db.json')
 
-
 //GET ALL the notes saved as objects in db.json file
 router.get('/notes', (req, res) => {
     console.log(`HTTP METHOD: ${req.method}`);
-    readFile('./db/db.json', 'utf-8', (err, data) => {  
+    readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
-        } 
+        }
         return res.json(JSON.parse(data));
     });
 });
@@ -22,26 +20,29 @@ router.post('/notes', (req, res) => {
     const { title, text } = req.body;
     console.log(`HTTP METHOD: ${req.method}`);
     //if the newNote has both a title and text:
-    if (title && text) {
-        const newNote = {
-            title, 
-            text,
-            id: uuidv1(),
+    readFile('./db/db.json', 'utf-8', (err, data) => {
+        const dbArray = JSON.parse(data);
+        if (title && text) {
+            const newNote = {
+                title,
+                text,
+                id: uuidv1(),
+            };
+
+            //add newNote to the array in db.json
+            dbArray.push(newNote);
+            // res.status(200).json(db);
+
+            //persist data into the db.json file
+            writeFile('./db/db.json', JSON.stringify(dbArray), (err) =>
+                err
+                    ? console.error(err)
+                    : console.log(
+                        `${newNote.title} has been written to JSON file`
+                    ));
         };
-
-        //add newNote to the array in db.json
-        db.push(newNote);
-        // res.status(200).json(db);
-
-        //persist data into the db.json file
-        writeFile('./db/db.json', JSON.stringify(db), (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `${newNote.title} has been written to JSON file`
-            ));
-    };
-    res.json({ messsage: 'Data received' });
+        res.json({ messsage: 'Data received' });
+    })
 });
 
 //DELETE a post matching the id chosen
@@ -55,30 +56,12 @@ router.delete('/notes/:id', (req, res) => {
             return res.status(500).send('Internal Server Error');
         }
 
-        let notes = JSON.parse(data);
-        // //findIndex, if no elements satisfy: -1,  otherwise will output the index number
-        // const deleteNoteIndex = notes.findIndex(note => note.id === noteId);
-        // if (deleteNoteIndex !== -1) {
-        //     const deletedNote = notes[deleteNoteIndex];
-        //     console.log(deletedNote);
-        //     notes.splice(deleteNoteIndex, 1);
-        //     console.log('Note was removed!');
-        //     //save new notes db
-        //     writeFile('./db/db.json', JSON.stringify(db), (err) =>
-        //     err
-        //       ? console.error(err)
-        //       : console.log(
-        //         `JSON file has been updated: '${deletedNote.title}' removed`
-        //         ));
-        // } else {
-        //     console.log(`notes_id ${noteId} not found`);
-        // }
-
+        const notes = JSON.parse(data);
         //filter OUT the object with the matching id; return array with rest of the objects
-        notes = notes.filter((note) => note.id !== noteId);
-        res.status(200).json(notes);
-        writeFile('./db/db.json', JSON.stringify(notes), (err) =>
-        err ? console.error(err) : console.log("removed"));
+        const noteDelete = notes.filter((note) => note.id !== noteId);
+        res.status(200).json(noteDelete);
+        writeFile('./db/db.json', JSON.stringify(noteDelete), (err) =>
+            err ? console.error(err) : console.log("removed"));
     })
 })
 
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const router = require('express').Router();
 const api = require('./routes/api');
 const html = require('./routes/html');
 
@@ -11,20 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use("/", html);
-
-//require('./routes/api')(app);
-//require('./routes/html')(app);
-
-//GET ONE of the saved notes to display
-// app.get('/api/notes/:note_id', (req, res) => {
-//     console.log(`HTTP METHOD: ${req.method}`);
-//     for (note of db) {
-//         if (db.note_id === req.params.note_id) {
-//             return res.json(note)
-//         }
-//     }
-// });
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`);
